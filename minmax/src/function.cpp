a86d23deb24034a83e83ca327c24bb58ce950024
Yago Gomes Da Silva <99800161+YagoGomesDaSilva@users.noreply.github.com>
2022-04-15 21:15:09 -0300

    Commit 6
    Atividade : Retângulo 1
    Correção : minmax

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -2,9 +2,14 @@
 
 std::pair<int, int> min_max(int V[], std::size_t n)
 {
-    int x = 0, aux_max = 0, aux_min = 0;
+    if (n == 0) {
+        return  std::make_pair(-1, -1);
+    }
+
+    int x = 0, aux_max = 0, aux_min = 0, min = 0, max = 0;
 
     std::pair<int, int> par;
+    std::pair<int, int> par_aux;
 
     par.first = V[0], par.second = V[0];
 
@@ -13,19 +18,21 @@ std::pair<int, int> min_max(int V[], std::size_t n)
 
             if (V[i] < V[j]) {
                 aux_min = V[i];
-                if (aux_min < par.first) {
-                    par.first = aux_min;
+                if (aux_min < par_aux.first) {
+                    par_aux.first = aux_min;
+                    min = i;
                 }
             }
             else if (V[i] > V[j]) {
                 aux_max = V[i];
-                if (aux_max > par.second) {
-                    par.second = aux_max;
+                if (aux_max > par_aux.second) {
+                    par_aux.second = aux_max;
+                    max = i;
                 }
             }
 
         }
     }
 
-    return par;
+    return std::make_pair(min, max);
 }
--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -6,8 +6,27 @@
 
 location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
-    // TODO: Coloque aqui seu código.
+    if ((IE).x < (P).x && (P).x < (SD).x && (IE).y < (P).y && (P).y < (SD).y) {
+        return INSIDE;
+    }
 
-    // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
-    return location_t::OUTSIDE;
+    else if ((P).x == (IE).x && (P).y >= (IE).y && (P).y <= (SD).y) {
+        return BORDER;//borda esquerda
+    }
+
+    else if ((P).x == (SD).x && (P).y >= (IE).y && (P).y <= (SD).y) {
+        return BORDER;//borda direita
+    }
+
+    else if ((P).y == (IE).y && (P).x >= (IE).x && (P).x <= (SD).x) {
+        return BORDER;//borda inferior
+    }
+
+    else if ((P).y == (SD).y && (P).x >= (IE).x && (P).x <= (SD).x) {
+        return BORDER;//borda superior
+    }
+
+    else {
+        return OUTSIDE;
+    }
 }
